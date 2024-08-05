01ebed7c0023fec5498f222f550799c6023e3055
caitlin-hartley <carmelcaitlin@berkeley.edu>
2024-08-05 13:18:31 -0600

    add app.py updates - import all modules, create engine, and start reflect" git push

--- surfs_up/app.py
+++ surfs_up/app.py
@@ -1,11 +1,30 @@
 # Import the dependencies.
 
+import numpy as np
+import pandas as pd
+import datetime as dt
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.inspection import inspect
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
 
 
 #################################################
 # Database Setup
 #################################################
 
+# create engine to hawaii.sqlite
+engine = create_engine("sqlite:///../resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+
+# reflect the tables
+Base.prepare(autoload_with=engine)
 
 # reflect an existing database into a new model
 
